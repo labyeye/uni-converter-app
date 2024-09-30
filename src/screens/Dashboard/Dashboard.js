3be3f86a0b25f993fa748137a4e3538142179f38
Labh Bothra <labhbothra1380@gmail.com>
2024-09-30 09:31:50 +0530

    Fixed DashBoard

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -1,28 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
-import { UserIcon, CalculatorIcon, ArrowPathIcon, FunnelIcon } from 'react-native-heroicons/solid';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+} from 'react-native';
+import {
+  UserIcon,
+  CalculatorIcon,
+  ArrowPathIcon,
+  FunnelIcon,
+} from 'react-native-heroicons/solid';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import IonIcons from 'react-native-vector-icons/Ionicons';
-import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
-import { ScrollView } from 'react-native-gesture-handler';
+import {BannerAd, BannerAdSize} from 'react-native-google-mobile-ads';
+import {ScrollView} from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 
-const Dashboard = ({ navigation }) => {
-
+const Dashboard = ({navigation}) => {
   const [input, setInput] = useState('');
   const screenWidth = Dimensions.get('window').width;
-  let buttonSize = (screenWidth <= 375) ? (screenWidth - 130) / 4 : (screenWidth - 70) / 3;
+  let buttonSize =
+    screenWidth <= 375 ? (screenWidth - 100) / 4 : (screenWidth - 180) / 3;
 
   const items = [
-    { name: 'Unit Converter', icon: <ArrowPathIcon color="black" size={buttonSize / 2} />, navigateTo: 'ConverterStack' },
-    { name: 'BMI', icon: <IonIcons name='body' color="black" size={buttonSize / 2} />, navigateTo: 'BodyMassIndex' },
-    { name: 'Simple Interest', icon: <MaterialCommunityIcons name='label-percent' size={buttonSize / 2} color='black' />, navigateTo: 'Simple' },
-    { name: 'Calculator', icon: <CalculatorIcon color="black" size={buttonSize / 2} />, navigateTo: 'Calculator' },
-    { name: 'Discount', icon: <MaterialCommunityIcons name='brightness-percent' size={buttonSize / 2} color='black' />, navigateTo: 'Discount' },
-    { name: 'Milage', icon: <MaterialCommunityIcons name='fuel' size={buttonSize / 2} color='black' />, navigateTo: 'Milage' },
-    { name: 'Matrix Multiply', icon: <MaterialCommunityIcons name='matrix' size={buttonSize / 2} color='black' />, navigateTo: 'MatrixMultiplication' },
-    { name: 'Multiply Inverse', icon: <MaterialCommunityIcons name='multiplication-box' size={buttonSize / 2} color='black' />, navigateTo: 'MultiplicativeInverse' },
-    { name: 'Gcd & Lcm', icon: <MaterialCommunityIcons name='math-norm-box' size={buttonSize / 2} color='black' />, navigateTo: 'GcdLcm' },
+    {
+      name: 'Unit Converter',
+      icon: <ArrowPathIcon color="black" size={buttonSize / 2} />,
+      navigateTo: 'ConverterStack',
+    },
+    {
+      name: 'BMI',
+      icon: <IonIcons name="body" color="black" size={buttonSize / 2} />,
+      navigateTo: 'BodyMassIndex',
+    },
+    {
+      name: 'Simple Interest',
+      icon: (
+        <MaterialCommunityIcons
+          name="label-percent"
+          size={buttonSize / 2}
+          color="black"
+        />
+      ),
+      navigateTo: 'Simple',
+    },
+    {
+      name: 'Calculator',
+      icon: <CalculatorIcon color="black" size={buttonSize / 2} />,
+      navigateTo: 'Calculator',
+    },
+    {
+      name: 'Discount',
+      icon: (
+        <MaterialCommunityIcons
+          name="brightness-percent"
+          size={buttonSize / 2}
+          color="black"
+        />
+      ),
+      navigateTo: 'Discount',
+    },
+    {
+      name: 'Milage',
+      icon: (
+        <MaterialCommunityIcons
+          name="fuel"
+          size={buttonSize / 2}
+          color="black"
+        />
+      ),
+      navigateTo: 'Milage',
+    },
+    {
+      name: 'Matrix Multiply',
+      icon: (
+        <MaterialCommunityIcons
+          name="matrix"
+          size={buttonSize / 2}
+          color="black"
+        />
+      ),
+      navigateTo: 'MatrixMultiplication',
+    },
+    {
+      name: 'Multiply Inverse',
+      icon: (
+        <MaterialCommunityIcons
+          name="multiplication-box"
+          size={buttonSize / 2}
+          color="black"
+        />
+      ),
+      navigateTo: 'MultiplicativeInverse',
+    },
+    {
+      name: 'Gcd & Lcm',
+      icon: (
+        <MaterialCommunityIcons
+          name="math-norm-box"
+          size={buttonSize / 2}
+          color="black"
+        />
+      ),
+      navigateTo: 'GcdLcm',
+    },
   ];
 
   const getGreeting = () => {
@@ -38,39 +122,43 @@ const Dashboard = ({ navigation }) => {
 
   return (
     <LinearGradient
-      colors={['#CDF5FD','#CDF5FD','white']}
-      style={styles.gradientContainer}
-    >
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.greetingText}>{getGreeting()}, User!</Text>
-        <View style={styles.buttonsContainer}>
-          {items.map((item, index) => (
-            <TouchableOpacity
-              key={index}
-              style={styles.itemContainer}
-              onPress={() => navigation.navigate(item.navigateTo)}
-            >
-              <View style={styles.buttonOuterShadow}>
-                <View style={styles.buttonInnerShadow}>
-                  <View style={[styles.neumorphButton, { width: buttonSize, height: buttonSize }]}>
-                    {item.icon}
+      colors={['#CDF5FD', '#CDF5FD', 'white']}
+      style={styles.gradientContainer}>
+      <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.content}>
+          <Text style={styles.greetingText}>{getGreeting()}, User!</Text>
+          <View style={styles.buttonsContainer}>
+            {items.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                style={styles.itemContainer}
+                onPress={() => navigation.navigate(item.navigateTo)}>
+                <View style={styles.buttonOuterShadow}>
+                  <View style={styles.buttonInnerShadow}>
+                    <View
+                      style={[
+                        styles.neumorphButton,
+                        {width: buttonSize, height: buttonSize},
+                      ]}>
+                      {item.icon}
+                    </View>
                   </View>
                 </View>
-              </View>
-              <Text style={styles.itemText}>{item.name}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
+                <Text style={styles.itemText}>{item.name}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
         <View style={styles.adContainer}>
           <BannerAd
-            unitId="ca-app-pub-6119758783032593/4123323972" // Replace with your actual Ad Unit ID
+            unitId="ca-app-pub-6119758783032593/4123323972" 
             size={BannerAdSize.FULL_BANNER}
             requestOptions={{
               requestNonPersonalizedAdsOnly: true,
             }}
           />
         </View>
-      </ScrollView>
+      </SafeAreaView>
     </LinearGradient>
   );
 };
@@ -80,14 +168,18 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
+    flex: 1,
+    justifyContent: 'space-between', 
+  },
+  content: {
     alignItems: 'center',
-    paddingBottom: 20, // Adjust the padding to avoid overlap with the ad banner
+    paddingBottom: 20, 
   },
   greetingText: {
     fontSize: 28,
-    color: "#00A9FF",
+    color: '#00A9FF',
     paddingTop: 20,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   buttonsContainer: {
     width: '100%',
@@ -106,13 +198,13 @@ const styles = StyleSheet.create({
   },
   buttonOuterShadow: {
     shadowColor: '#00A9FF',
-    shadowOffset: { width: 5, height: 6 },
+    shadowOffset: {width: 5, height: 6},
     shadowOpacity: 1,
     shadowRadius: 5,
   },
   buttonInnerShadow: {
     shadowColor: '#A0E9FF',
-    shadowOffset: { width: -6, height: -6 },
+    shadowOffset: {width: -6, height: -6},
     shadowOpacity: 0.7,
     shadowRadius: 6,
   },
@@ -122,6 +214,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     elevation: 4,
+    height: '10%',
+    width: '10%',
   },
   itemText: {
     fontSize: 13,
@@ -132,7 +226,9 @@ const styles = StyleSheet.create({
   adContainer: {
     width: '100%',
     alignItems: 'center',
-    marginTop: 20,
+    position: 'absolute',
+    bottom: 0,
+    paddingBottom: 10, // Adjust to add padding if needed
   },
 });
 
